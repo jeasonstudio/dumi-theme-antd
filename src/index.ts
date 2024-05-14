63701bf7ebc5af696474ffa89564875929f1bba3
KuangPF <me@kuangpf.com>
2024-05-14 20:08:32 +0800

    fix(main): defineThemeConfig 模块引入问题

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
 import Footer from './slots/Footer';
-import type { IAllThemeConfig } from './types';
 import Loading from './common/Loading';
 import HomeBaseLayout from './builtins/HomeBaseLayout';
 import GlobalLayout from './layouts/GlobalLayout';
-
-/**
- * @description provider declaration of config files
- * @param {ThemeConfig} config theme config
- * @returns {ThemeConfig}
- */
-export function defineThemeConfig(config: IAllThemeConfig): IAllThemeConfig {
-  return config;
-}
 
 export { Loading, Footer, HomeBaseLayout, GlobalLayout };
